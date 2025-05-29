5407dd5d9f4ab425bde2cbf64e8ba45a1faddc7c
doniyor209 <doniyortoshev123@gmail.com>
2025-05-29 14:22:16 +0500

    Add files via upload

---
+++ java.js
@@ -0,0 +1,32 @@
+const typed = new Typed('#ism', {
+    strings: ["Toshev Doniyor"],
+    typeSpeed: 150,
+    loop: true,
+    backSpeed: 100,
+    startDelay: 1000,
+    backDelay: 1000
+});
+const typed2 = new Typed('#p', {
+    strings: ["Make your headlines stand out"],
+    typeSpeed: 150,
+    loop: true,
+    backSpeed: 100,
+    startDelay: 1000,
+    backDelay: 1000
+});
+const typed3 = new Typed('#ism2', {
+    strings: ["Design is intelligence <br> made visible"],
+    typeSpeed: 150,
+    loop: true,
+    backSpeed: 100,
+    startDelay: 1000,
+    backDelay: 1000
+});
+const typed4 = new Typed('#ism3', {
+    strings: ["Interaction design is not merely a matter of aesthetic choice; rather, <br> it is based on an understanding of usersand cognitive principles."],
+    typeSpeed: 150,
+    loop: true,
+    backSpeed: 100,
+    startDelay: 1000,
+    backDelay: 1000
+});
